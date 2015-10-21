60f3e5835286b140c2cc768556590fddba2e3756
Alexander F Rødseth <rodseth@gmail.com>
2015-10-21 13:03:16 +0200

    Version bump
    Improve script output

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	maxCollectedWords = 2048
-	versionString     = "getver 0.33"
+	versionString     = "getver 0.4"
 
 	ALLOWED = "0123456789.-+_ABCDEFGHIJKLNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
